47c1b89c2acc7630555b31327e6b203cd738893c
Jonas Thoursie <jonas.thoursie@gmail.com>
2012-02-19 02:00:41 +0100

    Created user and microposts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
+  resources :members
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
